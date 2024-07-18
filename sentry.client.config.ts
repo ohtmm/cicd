7bfff922832084664c9545d707bb014900d54363
jiyoung <cwd05011@gmail.com>
2024-07-18 15:24:17 +0900

    update sentry setting

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,10 +2,11 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://f9769639af240e23b177d6546c1e903e@o4507620160110592.ingest.us.sentry.io/4507620160307201",
+  ignoreErrors: ['Something went wrong'],
+  dsn: 'https://f9769639af240e23b177d6546c1e903e@o4507620160110592.ingest.us.sentry.io/4507620160307201',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
